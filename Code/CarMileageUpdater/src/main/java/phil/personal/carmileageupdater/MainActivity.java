8952963e68fe5260b50696d08d33562c113e4ff9
Phil Azzi <philazzi44@gmail.com>
2013-09-28 22:52:40 -0400

    Version 1
    Pushes to a dummy pastebin account, and it basically works as expected
    (assuming network is available).

--- Code/CarMileageUpdater/src/main/java/phil/personal/carmileageupdater/MainActivity.java
+++ Code/CarMileageUpdater/src/main/java/phil/personal/carmileageupdater/MainActivity.java
@@ -3,13 +3,16 @@ package phil.personal.carmileageupdater;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -33,57 +36,85 @@ public class MainActivity extends Activity {
         EditText tboMileage = (EditText) findViewById(R.id.tboMileage);
 
         int newMileage = Integer.parseInt(tboMileage.getText().toString());
-
-        if (PostToPasteBin(newMileage))
-            ShowSuccess();
-        else
-            ShowFailure();
+        btnSubmit.setVisibility(View.INVISIBLE);
+        PostToPasteBin(newMileage);
     }
 
-    private boolean PostToPasteBin(Integer newMileage)
+    class PostToPasteBinTask extends AsyncTask<String, Void, Boolean> {
+
+        private Integer _newMileage;
+        public PostToPasteBinTask(Integer newMileage)
+        {
+            _newMileage = newMileage;
+        }
+        private Exception exception;
+
+        protected Boolean doInBackground(String... urls) {
+            try {
+                String apiDevKey = "1130938b77f534523710589cc6ae42c1";
+                String apiUserKey = "ccd2b7b197c8cab9104fc6d4dc665939";
+                String apiOption = "paste";
+                String apiPasteName = "carmileage";
+                String apiPasteExpireDate = "1W";
+                String apiPastePrivate = "1"; // Unlisted
+                String apiPasteCode = _newMileage.toString();
+
+                URL url = new URL("http://pastebin.com/api/api_post.php");
+                String urlParameters = "api_dev_key=" + apiDevKey +
+                        "&api_user_key=" + apiUserKey +
+                        "&api_option=" + apiOption +
+                        "&api_paste_name=" + apiPasteName +
+                        "&api_paste_expire_date=" + apiPasteExpireDate +
+                        "&api_paste_private=" + apiPastePrivate +
+                        "&api_paste_code=" + apiPasteCode;
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+                connection.setInstanceFollowRedirects(false);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                connection.setRequestProperty("charset", "utf-8");
+                connection.setRequestProperty("Content-Length", "" + Integer.toString(urlParameters.getBytes().length));
+                connection.setUseCaches (false);
+
+                DataOutputStream wr = new DataOutputStream(connection.getOutputStream ());
+                wr.writeBytes(urlParameters);
+                wr.flush();
+                wr.close();
+
+                // Get the response
+                BufferedReader rd = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
+
+                String allLine = "";
+                String line = "";
+                while ((line = rd.readLine()) != null) {
+                    allLine = allLine + line;
+                }
+
+                wr.close();
+                rd.close();
+                connection.disconnect();
+                return true;
+            } catch (Exception e) {
+                this.exception = e;
+                return false;
+            }
+        }
+
+        protected void onPostExecute(Boolean result) {
+            if (result)
+                ShowSuccess();
+            else
+                ShowFailure();
+            ((Button)findViewById(R.id.btnSubmit)).setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void PostToPasteBin(Integer newMileage)
     {
-        try
-        {
-            // see http://stackoverflow.com/questions/6343166/android-os-networkonmainthreadexception for async task fix
-            String apiDevKey = "1130938b77f534523710589cc6ae42c1";
-            String apiUserKey = "ccd2b7b197c8cab9104fc6d4dc665939";
-            String apiOption = "paste";
-            String apiPasteName = "carmileage";
-            String apiPasteExpireDate = "1W";
-            String apiPastePrivate = "1"; // Unlisted
-            String apiPasteCode = newMileage.toString();
-            
-            URL url = new URL("http://pastebin.com/api/api_post.php");
-            String urlParameters = "api_dev_key=" + apiDevKey + 
-                                   "&api_user_key=" + apiUserKey +
-                                   "&api_option=" + apiOption +
-                                   "&api_paste_name=" + apiPasteName +
-                                   "&api_paste_expire_date=" + apiPasteExpireDate +
-                                   "&api_paste_private=" + apiPastePrivate +
-                                   "&api_paste_code=" + apiPasteCode;
-            
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            connection.setInstanceFollowRedirects(false); 
-            connection.setRequestMethod("POST"); 
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded"); 
-            connection.setRequestProperty("charset", "utf-8");
-            connection.setRequestProperty("Content-Length", "" + Integer.toString(urlParameters.getBytes().length));
-            connection.setUseCaches (false);
-            
-            DataOutputStream wr = new DataOutputStream(connection.getOutputStream ());
-            wr.writeBytes(urlParameters);
-            wr.flush();
-            wr.close();
-            connection.disconnect();
-            return true;
-        }
-        catch (Exception e)
-        {
-             e.printStackTrace();
-             return false;
-        }
+        new PostToPasteBinTask(newMileage).execute();
     }
 
     private void ShowSuccess()
